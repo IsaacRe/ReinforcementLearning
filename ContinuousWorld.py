c52fff3696fab4dfa65d9ca220b1a43df6011baf
isaac <isaacrehg@gmail.com>
2018-01-02 14:20:33 -0500

    Start continuous world framework

---
+++ ContinuousWorld.py
@@ -0,0 +1,17 @@
+import Random
+from abc import ABCMeta, abstractmethod
+
+class DiscActionTargetWorld(DiscreteActionWorld):
+
+class DiscreteActionWorld(World):
+    __metaclass__ = ABCMeta
+
+
+class World:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, ):
+        pass
+
+    def timestep(self, ):
+
